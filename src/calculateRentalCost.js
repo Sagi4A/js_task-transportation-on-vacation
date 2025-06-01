1fe5fad166880094cd92254a0e1d0b16e998d657
Sagi4A <voitko.katya.1@gmail.com>
2025-06-01 18:19:17 +0200

    sol

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const START_PRICE = 40;
+  const LONG_TERM = 7;
+  const MID_TERM = 3;
+  const DISCOUNT_FOR_LONG_TERM = 50;
+  const DISCOUNT_FOR_MID_TERM = 20;
+  const totalAmount = START_PRICE * days;
+
+  if (days === LONG_TERM) {
+    return totalAmount - DISCOUNT_FOR_LONG_TERM;
+  }
+
+  if (days >= MID_TERM) {
+    return totalAmount - DISCOUNT_FOR_MID_TERM;
+  }
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
